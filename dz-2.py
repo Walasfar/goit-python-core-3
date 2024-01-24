479b5958611330d956b35a596c09f08f3d31f8f4
Walasfar <quote.andrew@gmail.com>
2024-01-24 11:58:57 +0100

    second homme work

--- dz-2.py
+++ dz-2.py
@@ -3,8 +3,11 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    list_of_numbers = random.sample(range(min, max), quantity)
-    list_of_numbers.sort()
-    return list_of_numbers
+    if min >= 1 and max <= 1000:
+        list_of_numbers = random.sample(range(min, max), quantity) # Create list of random uniq numbers
+        list_of_numbers.sort() # sort
+        return list_of_numbers
+    else: # If numbers range bad provided
+        print("Задано неправельний діапазон чисел!")
 
-print(get_numbers_ticket(1, 48, 6))
+print(get_numbers_ticket(1, 1000, 6))
