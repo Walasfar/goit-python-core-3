cdf7fcd97579ccd5c0737f255e3ef747946d3e78
Walasfar <quote.andrew@gmail.com>
2024-01-27 21:20:41 +0100

    Переробив блок except

--- dz-1.py
+++ dz-1.py
@@ -1,22 +1,24 @@
 # Home work III (1)
 
-from datetime import datetime as dt # Імпортуємо модулі - as dt це псивдонім datetime
+from datetime import datetime as dt 
 
-# Argument example: yyy-dd-mm
-def get_days_from_today(enter_date): # Передаемо аргумент ввиді дати
+
+def get_days_from_today(enter_date):
     
     try: # Провіряємо введену дату
         
-        user_date = dt.strptime(enter_date, "%Y-%m-%d") # Змінна з датою, якщо дата введена неправильно - вона йде в блок except
-        now = dt.today() # Согоднішній день
-        delta_days = now - user_date # Скільки днів пройшло або ще не настало
+        user_date = dt.strptime(enter_date, "%Y-%m-%d")
+        now = dt.today() 
+        delta_days = now - user_date
     
-    except ValueError: # Якщо try не розібрав дату то виведеться це повідомлення
-        print("The entered event date is not suitable", ValueError)
+    except ValueError as e: # Якщо try не розібрав дату то виведеться це повідомлення
+        
+        print("The entered event date is not suitable", e)
+        return
         
     return delta_days.days # Повертаємо кількість днів
 
 
-num = get_days_from_today("2024-01-27") # Присвоюємо змінній num дні, які вернула наша функція
+num = get_days_from_today("2024-01-27")
 
-print(num) # Вивід
+print(num)
