cf8d2970cdb372bc35580bf235e2984e9695004e
Walasfar <quote.andrew@gmail.com>
2024-01-27 16:42:46 +0100

    Рефактор та коментарі

--- dz-2.py
+++ dz-2.py
@@ -2,12 +2,15 @@
 
 import random
 
-def get_numbers_ticket(min_tickets, max_tickets, quantity):
-    if min_tickets >= 1 and max_tickets <= 1000:
-        list_of_numbers = random.sample(range(min_tickets, max_tickets), quantity) # Create list of random uniq numbers
-        list_of_numbers.sort() # sort
-        return list_of_numbers
+def get_numbers_ticket(min_number, max_number, quantity):
+    
+    if min_number >= 1 and max_number <= 1000: # Числа мають бути більше 0 і мін рівно 1000
+        # Завдяки random гереруємо список з  різними унікальними числами.
+        list_of_numbers = random.sample(range(min_number, max_number), quantity)
+        list_of_numbers.sort() # Сортуємо числа.
+        return list_of_numbers # Повертаємо список з числами.
+    
     else: # If numbers range bad provided
-        print("Задано неправельний діапазон чисел!")
+        print("Введено неправильний діапазон!")
 
 print(get_numbers_ticket(1, 1000, 6))
