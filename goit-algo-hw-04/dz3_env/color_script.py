466989989dfa1329069e1ee0c036593a4b05d739
Walasfar <quote.andrew@gmail.com>
2024-01-31 20:07:56 +0100

    Поправив параметер

--- goit-algo-hw-04/dz3_env/color_script.py
+++ goit-algo-hw-04/dz3_env/color_script.py
@@ -11,26 +11,26 @@ item_back = Back.LIGHTCYAN_EX
 item_color = Fore.BLACK
 clear = Style.RESET_ALL
 
-def main(user_path, indent=' ' * 1):
+def main(user_path, indent=' '):
     """
     user_path - початковий путь
     indent - параметер за замовчуванням, кількість відступів - рекурсія
     """
     path = Path(user_path)
-
+    
     if path.exists():
-        # Проходимося по папці
         
+        # Проходимося по папці
         for item in path.iterdir():    
-            if item.is_file(): 
+            if item.is_file(): # Перевірка на файл
                 print(indent, item_back + item_color + str(item.name) + clear)
                 
-            elif item.is_dir():
+            elif item.is_dir(): # Перевірка на папку
                 print(indent, folder_back + folder_color + str(item.name) + ' ↴' + Style.RESET_ALL)
                 main(item, indent * 2 )
     else:
         print("File or direction is not exists.")
-        
+
 
 
 if __name__ == '__main__':
