af69ac2eeaa8266704646507cd8a9e8d1794e600
Walasfar <quote.andrew@gmail.com>
2024-01-31 16:31:32 +0100

    Поправки

--- goit-algo-hw-04/dz4.py
+++ goit-algo-hw-04/dz4.py
@@ -1,76 +1,65 @@
-
+# Приймає команду юзера
 def parse_input(user_input):
     cmd, *args = user_input.split()
-    cmd = cmd.strip().lower()        
+    cmd = cmd.strip().lower()  
     return cmd, *args
 
+# Добавляє контакт в базу
 def add_contact(args, contacts):
     name, phone = args
     contacts[name] = phone
     return "Contact added"
 
+# Міняє номер
 def change_number(args, contacts):
     name, new_number = args
     if name in contacts:
         contacts[name] = new_number
-        return "Contact update."
+        return "Contact updated."
     else:
         return "Name not found!"
 
+def show_base(adress_book):
+    for key, value in adress_book.items():
+        print(f"Name: {key}, number: {value}")
+
 def main():
-    
     contacts = {}
     print("Welcome to the assistant bot!")
     
     while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
         
-            user_input = input("Enter a command: ")
-            command, *args = parse_input(user_input)
-            name = args[0]
+        try:
             
-            if command in ["close", "exit"]:
+            if command in ["close", "quit", "exit"]:
                 print("Good bye!")
                 break
+            elif command == 'hello':
+                print("Hello im here for help you!")
             
-            try:
-                match command:
-                    
-                    case "all":
-                        
-                        if len(contacts) > 0:
-                            
-                            for name, number in contacts.items():
-                                print(f"Ім'я: {name}, номер: {number}")
-                        
-                        else:
-                            print("Book contacts is empty.")
-                        
-                    case "hello":
-                        print("How can I help you?")
-                        
-                    case "add":
-                        if name in contacts:
-                            print("Name exists!")
-                        else:
-                            print(add_contact(args, contacts))
-                    
-                    case "change":
-                        print(change_number(args, contacts))
-                    
-                    case "phone":
-                        if name in contacts:
-                            print(f"Phone {name}: ", contacts[args[0]])
-                        else:
-                            print("Contact not found.")
-                    
-                    case _:
-                        print("Invalid command.")
-                    
-            except ValueError as e:
-                print(f"You enter bad commands! Need: command name number")
+            elif command == 'all':
+            
+                if len(contacts) == 0:
+                    print("Base is empty.")
+                else:
+                    show_base(contacts)
                 
-    return contacts
-
+            elif command == 'add':
+                add_contact(args, contacts)
+                
+            match command:
+                
+                case 'change':
+                    print (change_number(args, contacts))
+                    
+                case 'phone':
+                    print(f"Phone {args[0]} - {contacts[args[0]]}")
+                
+        except ValueError as e:
+            print("Number argumets is required!")
+        
 
 if __name__ == "__main__":
     main()
