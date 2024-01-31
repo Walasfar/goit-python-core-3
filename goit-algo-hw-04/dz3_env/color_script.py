2bd6429854920d24e519aaa371d275e0bdc5737d
Walasfar <quote.andrew@gmail.com>
2024-01-31 14:24:43 +0100

    Добавив перевірку на існування файла

--- goit-algo-hw-04/dz3_env/color_script.py
+++ goit-algo-hw-04/dz3_env/color_script.py
@@ -1,24 +1,37 @@
 import sys
-import sys
-from pathlib import PurePath, Path
+from pathlib import Path
 from colorama import init, Fore, Back, Style
 
 init()
 
+# Стилі
+folder_back = Back.YELLOW
+folder_color = Fore.BLACK
+item_back = Back.LIGHTCYAN_EX
+item_color = Fore.BLACK
+clear = Style.RESET_ALL
 
-def main(start_path, indent=' ' * 1):
-    
-    path = Path(start_path)
+def main(user_path, indent=' ' * 1):
+    """
+    user_path - початковий путь
+    indent - параметер за замовчуванням, кількість відступів - рекурсія
+    """
+    path = Path(user_path)
 
-    # Проходимося по папці
-    for item in path.iterdir():
-        if item.is_file(): 
-            print(indent, Back.LIGHTBLACK_EX + Fore.LIGHTYELLOW_EX + str(item.name) + Style.RESET_ALL)
-        elif item.is_dir():
-            print(indent, Back.LIGHTBLACK_EX + Fore.LIGHTCYAN_EX + str(item.name) + ' ↴' + Style.RESET_ALL)
-            main(item, indent * 2 )
+    if path.exists():
+        # Проходимося по папці
+        
+        for item in path.iterdir():    
+            if item.is_file(): 
+                print(indent, item_back + item_color + str(item.name) + clear)
+                
+            elif item.is_dir():
+                print(indent, folder_back + folder_color + str(item.name) + ' ↴' + Style.RESET_ALL)
+                main(item, indent * 2 )
+    else:
+        print("File or direction is not exists.")
+        
+
 
 if __name__ == '__main__':
     main(str(sys.argv[1]))
-
-
