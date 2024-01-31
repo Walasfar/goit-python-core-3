ca28c036b6d025818ce129cf5863f8c91748c07a
Walasfar <quote.andrew@gmail.com>
2024-01-31 12:38:08 +0100

    Закінчив свій explorer

---
+++ goit-algo-hw-04/dz3_env/color_script.py
@@ -0,0 +1,23 @@
+import sys
+import sys
+from pathlib import PurePath, Path
+from colorama import init, Fore, Back, Style
+
+init()
+
+
+def main(start_path, indent=' ' * 1):
+    
+    path = Path(start_path)
+        
+    for item in path.iterdir():
+        if item.is_file():
+            print(indent, Back.LIGHTBLACK_EX + Fore.LIGHTYELLOW_EX + str(item.name) + Style.RESET_ALL)
+        elif item.is_dir():
+            print(indent, Back.LIGHTBLACK_EX + Fore.LIGHTCYAN_EX + str(item.name) + ' ↴' + Style.RESET_ALL)
+            main(item, indent * 2 )
+
+if __name__ == '__main__':
+    main(str(sys.argv[1]))
+
+
