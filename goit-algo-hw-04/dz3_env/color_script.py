e524e5e12f877e992454857829d6576dcd94fe39
Walasfar <quote.andrew@gmail.com>
2024-01-31 12:41:46 +0100

    зробив свій explorer

--- goit-algo-hw-04/dz3_env/color_script.py
+++ goit-algo-hw-04/dz3_env/color_script.py
@@ -9,9 +9,10 @@ init()
 def main(start_path, indent=' ' * 1):
     
     path = Path(start_path)
-        
+
+    # Проходимося по папці
     for item in path.iterdir():
-        if item.is_file():
+        if item.is_file(): 
             print(indent, Back.LIGHTBLACK_EX + Fore.LIGHTYELLOW_EX + str(item.name) + Style.RESET_ALL)
         elif item.is_dir():
             print(indent, Back.LIGHTBLACK_EX + Fore.LIGHTCYAN_EX + str(item.name) + ' ↴' + Style.RESET_ALL)
